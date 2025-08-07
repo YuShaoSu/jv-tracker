eb951d8f5a63e1d2dd41e1f6693c294ca72784de
YuShaoSu <n270882@gmail.com>
2025-08-07 13:47:22 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JapaneseVocabTracker from './components/JapaneseVocabTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <JapaneseVocabTracker />
+    </div>
+  );
+}
+
+export default App;
